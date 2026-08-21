e41bea6522946979668159a85c20f86961cbf151
fiorellaPErezM <fiorellaperez608@gmail.com>
2026-08-20 23:19:57 -0500

    Add files via upload

---
+++ Services/orden-ms/src/main/java/pe/edu/upeu/orden_ms/OrdenMsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.orden_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenMsApplication.class, args);
+	}
+
+}
